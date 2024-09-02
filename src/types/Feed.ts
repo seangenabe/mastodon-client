4c893cd2fe69d523f3ddb07582a1bf677c490ce9
Sean Marvi Oliver Genabe <seanmarviolivergenabe@gmail.com>
2024-09-03 07:45:34 +0800

    rename view to feed

--- src/types/Feed.ts
+++ src/types/Feed.ts
@@ -4,7 +4,7 @@ import type { PostGroup } from "@/types/PostGroup";
 /**
  * A viewable user-selectable view of social media posts with a title.
  */
-export interface View {
+export interface Feed {
   title: string;
   viewItem: (Post | PostGroup)[]
 }
