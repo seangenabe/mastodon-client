587fdc59c1ad4c9792c05743b417408e7ad09bd4
Sean Marvi Oliver Genabe <seanmarviolivergenabe@gmail.com>
2024-09-23 05:41:25 +0800

    account type

--- src/types/Account.ts
+++ src/types/Account.ts
@@ -1,11 +1,17 @@
 import type { mastodon } from "masto";
 
-export interface Account {
+export interface CommonAccount {
   username: string;
   instanceName: string;
+}
+
+export interface MastodonAccount extends CommonAccount {
+  type: "mastodon";
   accessToken?: string;
   accountInfo?: mastodon.v1.Account;
 }
+
+export type Account = MastodonAccount;
 
 export function toString(a: Account) {
   return `${a.username}@${a.instanceName}`;
