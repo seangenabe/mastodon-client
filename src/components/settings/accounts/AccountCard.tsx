2e17d08799c3cf14b613615e7635c71e22643d58
Sean Marvi Oliver Genabe <seanmarviolivergenabe@gmail.com>
2024-09-23 04:18:19 +0800

    show account avatar

--- src/components/settings/accounts/AccountCard.tsx
+++ src/components/settings/accounts/AccountCard.tsx
@@ -1,6 +1,7 @@
 import { AccountLine } from "@/components/common/AccountLine";
 import Button from "@/components/common/Button";
 import { type Account, toString } from "@/types/Account";
+import { Show } from "solid-js";
 
 export default function AccountCard({
   accountKey,
@@ -12,8 +13,25 @@ export default function AccountCard({
   onDeleteClick?(accountKey: string, account: Account): void;
 }) {
   return (
-    <div class="flex flex-col md:min-h-32 bg-ctp-surface1 rounded-lg p-4">
+    <div
+      class="flex flex-col md:min-h-32 bg-ctp-surface1 rounded-lg p-4"
+      style={{
+        "background-image": `url(${
+          JSON.stringify(account.accountInfo?.header)
+        })`,
+      }}
+    >
       <div class="flex-1">
+        <Show when={account.accountInfo?.avatar}>
+          <figure>
+            <img
+              src={account.accountInfo?.avatar}
+              alt={`${toString(account)}'s avatar`}
+              width={80}
+              class="rounded-lg border-ctp-overlay0 border-4 border-solid"
+            />
+          </figure>
+        </Show>
         <AccountLine account={account} />
       </div>
       <div class="text-sm md:justify-end flex flex-row gap-4">
